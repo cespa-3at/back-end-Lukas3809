b73506d263700139f5593a457a6f3afceb0a0e01
Lukas3809 <lukas.bacili.rodrigues@escola.pr.gov.br>
2026-03-27 11:50:31 +0000

    commit

--- server.js
+++ server.js
@@ -7,30 +7,30 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 
 // Conecta ao banco de dados (certifique-se de que o nome do arquivo .db está correto)
-const db = new sqlite3.Database('./seu_nome.db');
+const db = new sqlite3.Database('./lukas.db');
 
 // ROTA PRINCIPAL: Abre o formulário (index.html) quando você acessa o site
 app.get('/', (req, res) => {
- res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // ROTA DE SALVAMENTO: Recebe os dados e grava no SQLite
 app.post('/salvar', (req, res) => {
- const { nome, telefone, email } = req.body;
- const sql = `INSERT INTO contatos (nome, telefone, email) VALUES (?, ?, ?)`;
+    const { nome, telefone, email } = req.body;
+    const sql = `INSERT INTO contatos (nome, telefone, email) VALUES (?, ?, ?)`;
 
- db.run(sql, [nome, telefone, email], function(err) {
-     if (err) {
-         return res.send("<h1>Erro ao salvar!</h1><p>" + err.message + "</p><a href='/'>Voltar</a>");
-     }
-     res.send("<h1>Sucesso!</h1><p>Contato salvo no banco.</p><a href='/'>Voltar e cadastrar outro</a>");
- });
+    db.run(sql, [nome, telefone, email], function(err) {
+        if (err) {
+            return res.send("<h1>Erro ao salvar!</h1><p>" + err.message + "</p><a href='/'>Voltar</a>");
+        }
+        res.send("<h1>Sucesso!</h1><p>Contato salvo no banco.</p><a href='/'>Voltar e cadastrar outro</a>");
+    });
 });
 
 // Inicia o servidor na porta 3000
 app.listen(3000, () => {
- console.log("------------------------------------------");
- console.log("Servidor rodando em http://localhost:3000");
- console.log("Pressione Ctrl + C para parar o servidor");
- console.log("------------------------------------------");
+    console.log("------------------------------------------");
+    console.log("Servidor rodando em http://localhost:3000");
+    console.log("Pressione Ctrl + C para parar o servidor");
+    console.log("------------------------------------------");
 });
